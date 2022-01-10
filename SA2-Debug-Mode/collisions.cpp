b656cda746e2cea622bfdb6d78e7c0004acb5be2
Sora <sora.x@hotmail.fr>
2022-01-10 17:06:11 +0100

    ACTUALLY Remove the Chao cam col

--- SA2-Debug-Mode/collisions.cpp
+++ SA2-Debug-Mode/collisions.cpp
@@ -61,11 +61,10 @@ void __cdecl DrawCollisionInfo(CollisionInfo* ColInfo)
 void __cdecl DrawDebugCollision(ObjectMaster* a1)
 {
 
-	if (a1->Data1.Entity->Collision == nullptr || !isColDebug || a1->MainSub == (ObjectFuncPtr)0x7af81030) //don't display the giant collision in Chao Garden
+	if (a1->Data1.Entity->Collision == nullptr || !isColDebug || a1->MainSub == (ObjectFuncPtr)0x7af81030 || a1->MainSub == (ObjectFuncPtr)0x57CD50) //don't display the giant collision in Chao Garden
 		return;
 
 	DrawCollisionInfo(a1->Data1.Entity->Collision);
-
 }
 
 void CheckAndDisplayCol(ObjectMaster* a1) {
